71cb4e7885f376227efb37f63eb1149570878a33
RIOT <62182170+RIOTTREU@users.noreply.github.com>
2022-10-22 21:54:26 +0700

    Add files via upload

---
+++ LBE3/E3LB3.lua
@@ -0,0 +1,32 @@
+function LB1()
+    gg.clearResults()
+    gg.searchNumber("11001~20008;0~255;-1::9",4)
+    gg.refineNumber("11001~20008",4)
+    RR = gg.getResults(1)
+    gg.clearResults()
+    RR1 = {{["address"] = RR[1].address -104,["flags"] = 4,["name"] = "map_info"},}
+    gg.addListItems(RR1)
+    RR2 = {
+        {["address"] = RR[1].address -100,["flags"] = 4,["value"] = "31960"},
+        {["address"] = RR[1].address -88,["flags"] = 4,["value"] = "17920"},
+        {["address"] = RR[1].address -84,["flags"] = 4,["value"] = "17408"},
+        {["address"] = RR[1].address + 204,["flags"] = 4,["value"] = "99999999"},
+    }
+    while true do
+        GG = gg.getListItems()
+        for i = 1,#GG do
+            if GG[i] ["name"] == "map_info" and GG[i] ["value"] ~= "31990" then
+                if GG[i] ["name"] == "map_info" and GG[i] ["value"] == "31960" then
+                else 
+                    gg.setValues(RR2)
+                end
+            end
+        end
+    end
+end
+while true do
+    if gg.isVisible(true) then
+        gg.setVisible(false)
+        LB1()
+    end
+end
---
+++ LBE3/E3LB4.lua
@@ -0,0 +1,32 @@
+function LB1()
+    gg.clearResults()
+    gg.searchNumber("11001~20008;0~255;-1::9",4)
+    gg.refineNumber("11001~20008",4)
+    RR = gg.getResults(1)
+    gg.clearResults()
+    RR1 = {{["address"] = RR[1].address -104,["flags"] = 4,["name"] = "map_info"},}
+    gg.addListItems(RR1)
+    RR2 = {
+        {["address"] = RR[1].address -100,["flags"] = 4,["value"] = "32260"},
+        {["address"] = RR[1].address -88,["flags"] = 4,["value"] = "13568"},
+        {["address"] = RR[1].address -84,["flags"] = 4,["value"] = "18176"},
+        {["address"] = RR[1].address + 204,["flags"] = 4,["value"] = "99999999"},
+    }
+    while true do
+        GG = gg.getListItems()
+        for i = 1,#GG do
+            if GG[i] ["name"] == "map_info" and GG[i] ["value"] ~= "32290" then
+                if GG[i] ["name"] == "map_info" and GG[i] ["value"] == "32260" then
+                else 
+                    gg.setValues(RR2)
+                end
+            end
+        end
+    end
+end
+while true do
+    if gg.isVisible(true) then
+        gg.setVisible(false)
+        LB1()
+    end
+end
